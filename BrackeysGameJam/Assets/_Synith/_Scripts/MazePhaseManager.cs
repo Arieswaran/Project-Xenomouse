8176058c514e763a505786cdeb94e5760e642e8d
synith <synith@live.ca>
2023-02-15 17:52:22 -0700

    On Cheese collected, increase game manager cheese

--- BrackeysGameJam/Assets/_Synith/_Scripts/MazePhaseManager.cs
+++ BrackeysGameJam/Assets/_Synith/_Scripts/MazePhaseManager.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 
 public class MazePhaseManager : MonoBehaviour
 {
+    public static MazePhaseManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
     private void Start()
     {
         Player.OnMouseDeath += Player_OnMouseDeath;
@@ -14,9 +25,23 @@ public class MazePhaseManager : MonoBehaviour
 
     private void Player_OnMouseDeath()
     {
-        if (GameManager.instance != null)
-        {
-            GameManager.instance.GenerateNextGenerationMouse();
-        }
+        if (GameManager.instance == null) return;
+        GameManager.instance.GenerateNextGenerationMouse();
+    }
+
+    public void IncreaseRedCheeseCount()
+    {
+        if (GameManager.instance == null) return;
+        GameManager.instance.IncreaseRedCheese();
+    }
+    public void IncreaseGreenCheeseCount()
+    {
+        if (GameManager.instance == null) return;
+        GameManager.instance.IncreaseGreenCheese();
+    }
+    public void IncreaseBlueCheeseCount()
+    {
+        if (GameManager.instance == null) return;
+        GameManager.instance.IncreaseBlueCheese();
     }
 }
